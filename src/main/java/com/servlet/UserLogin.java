e8d0963788466b237b8f50b2524517871fb5093d
ToodlyDave <alapatdavid@gmail.com>
2022-01-22 14:50:54 +0530

    CREATE login page and servlet

---
+++ src/main/java/com/servlet/UserLogin.java
@@ -0,0 +1,47 @@
+package com.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(
+		urlPatterns = "/UserLogin",
+		initParams = {
+				@WebInitParam(name = "user", value = "David"),
+				@WebInitParam(name = "password", value = "123")
+		}
+)
+public class UserLogin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String user = getServletConfig().getInitParameter("user");
+		String password = getServletConfig().getInitParameter("password");
+		
+		String userInput = request.getParameter("user");
+		String pwdInput = request.getParameter("password");
+		
+		System.out.println(" user: " + user + " input: " + userInput);
+		System.out.println(" pwd: " + password + " input: " + pwdInput);
+		
+		if (user.equalsIgnoreCase(userInput) && password.equalsIgnoreCase(pwdInput)) {
+			System.out.println(" Correct user");
+			request.setAttribute("user", user);
+			request.getRequestDispatcher("/LoginSuccess.jsp").forward(request, response);
+		}
+		
+		else {
+			System.out.println(" Incorrect user");
+			RequestDispatcher rd = getServletContext().getRequestDispatcher("/index.jsp");
+			rd.include(request, response);
+		}
+	}
+
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,15 @@
 <html>
 <body>
-	hi there
+	<form action="UserLogin" method="post">
+		<div>
+			User name: <input type="text" name="user">
+		</div>
+		<div>
+			Password: <input type="password" name="password">
+		</div>
+		<div>
+			<input type="submit" value="LOGIN">
+		</div>
+	</form>
 </body>
 </html>
